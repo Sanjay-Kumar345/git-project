bbc2ebde7b1af657a869de7b318eb7fde01b55c1
Sanjay-Kumar345 <sanjaykumar345@gmail.com>
2026-06-01 16:10:30 +0530

    adding storage account

--- resource.tf
+++ resource.tf
@@ -4,3 +4,13 @@ resource "azurerm_resource_group" "rgs"{
 
 }
   
+resource "azuerm_storage_account"{
+    name = "kddi-strg"
+    location = azurerm_resource_group.rgs.location
+    resource_group_name = azurerm_resource_group.rgs.name
+    account_tier = "Standard"
+    account_replication_type = "GRS"
+
+
+}
+  
